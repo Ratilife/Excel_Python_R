bed2ecdfc73a2ecc1b7f7ebca24cb527da8b6fa7
Ratilife <ratilife81@gmail.com>
2026-01-05 20:08:32 +0300

    переписан модуль добавлены методы чтения данных

--- src/functionality_for_Excel/functionality_for_Excel.py
+++ src/functionality_for_Excel/functionality_for_Excel.py
@@ -1,32 +1,74 @@
-
-from openpyxl import load_workbook
+import openpyxl
 import pandas as pd
 
-def read_single_sheet_DataFrame(workbook, sheet_name):
-        '''Прочитать один лист и создать DataFrame на основе второй и последующих строк данных с заголовками в качестве названий столбцов'''
+def read_excel_sheet(file_path, sheet_name):
+    """
+    Читает данные с указанного листа Excel-файла и возвращает их в виде pandas DataFrame.
+    Предполагается, что первая строка содержит заголовки столбцов.
 
-        # Загрузить лист из рабочей книги
-        sheet = workbook[sheet_name]
+    :param file_path: Путь к Excel-файлу.
+    :param sheet_name: Имя листа для чтения.
+    :return: pandas DataFrame с данными из листа.
+    """
+    # Загрузить рабочую книгу
+    wb = openpyxl.load_workbook(file_path)
 
-        # Прочитать необработанные данные, включая заголовки
+    # Выбрать лист
+    sheet = wb[sheet_name]
+
+    # Извлечь значения (включая заголовки)
+    sheet_data_raw = sheet.values
+
+    # Разделить заголовки в отдельную переменную
+    header = next(sheet_data_raw)[0:]
+
+    # Создать DataFrame на основе оставшихся строк данных с заголовками в качестве названий столбцов
+    df = pd.DataFrame(sheet_data_raw, columns=header)
+
+    # Закрыть рабочую книгу для освобождения ресурсов
+    wb.close()
+
+    return df
+
+
+def read_all_excel_sheets(file_path):
+    """
+    Читает данные со всех листов Excel-файла и объединяет их в один pandas DataFrame.
+    Предполагается, что на всех листах структура данных одинакова (одинаковые заголовки в первой строке).
+
+    :param file_path: Путь к Excel-файлу.
+    :return: pandas DataFrame с объединёнными данными из всех листов.
+    """
+    # Загрузить рабочую книгу
+    wb = openpyxl.load_workbook(file_path)
+
+    # Получить список всех имён листов
+    sheet_names = wb.sheetnames
+
+    # Список для хранения DataFrame'ов с каждого листа
+    dataframes = []
+
+    # Пройти по каждому листу
+    for sheet_name in sheet_names:
+        # Выбрать лист
+        sheet = wb[sheet_name]
+
+        # Извлечь значения (включая заголовки)
         sheet_data_raw = sheet.values
 
-        # Отделить заголовки в отдельную переменную
-        columns = next(sheet_data_raw)[0:]
+        # Разделить заголовки в отдельную переменную
+        header = next(sheet_data_raw)[0:]
 
-        # Создать DataFrame на основе второй и последующих строк данных с заголовками в качестве названий столбцов и вернуть его
-        return pd.DataFrame(sheet_data_raw, columns=columns)
+        # Создать DataFrame на основе оставшихся строк данных с заголовками в качестве названий столбцов
+        df = pd.DataFrame(sheet_data_raw, columns=header)
 
+        # Добавить DataFrame в список
+        dataframes.append(df)
 
-def read_multiple_sheets_DataFrame(file_path):
-    '''Прочитать все листы книги и объединить их в один DataFrame'''
+    # Закрыть рабочую книгу для освобождения ресурсов
+    wb.close()
 
-    # Загрузить рабочую книгу
-    workbook = load_workbook(file_path)
+    # Объединить все DataFrame'ы в один, игнорируя исходные индексы
+    consolidated_df = pd.concat(dataframes, ignore_index=True)
 
-    # Получить список всех названий листов в рабочей книге
-    sheet_names = workbook.sheetnames
-
-    # Перебрать названия листов, загрузить данные для каждого и объединить их в один DataFrame
-    return pd.concat([read_single_sheet(workbook=workbook, sheet_name=sheet_name) for sheet_name in sheet_names], ignore_index=True)
-
+    return consolidated_df
