d1dc56be29ba0a2930beaaf59f8189f3e1ddee4f
Ratilife <ratilife81@gmail.com>
2026-01-05 18:50:55 +0300

    Создание нового модуля

---
+++ src/functionality_for_Excel/functionality_for_Excel.py
@@ -0,0 +1,30 @@
+
+from openpyxl import load_workbook
+import pandas as pd
+
+def read_single_sheet(workbook, sheet_name):
+
+        # Загрузить лист из рабочей книги
+        sheet = workbook[sheet_name]
+
+        # Прочитать необработанные данные, включая заголовки
+        sheet_data_raw = sheet.values
+
+        # Отделить заголовки в отдельную переменную
+        columns = next(sheet_data_raw)[0:]
+
+        # Создать DataFrame на основе второй и последующих строк данных с заголовками в качестве названий столбцов и вернуть его
+        return pd.DataFrame(sheet_data_raw, columns=columns)
+
+
+def read_multiple_sheets(file_path):
+
+    # Загрузить рабочую книгу
+    workbook = load_workbook(file_path)
+
+    # Получить список всех названий листов в рабочей книге
+    sheet_names = workbook.sheetnames
+
+    # Перебрать названия листов, загрузить данные для каждого и объединить их в один DataFrame
+    return pd.concat([read_single_sheet(workbook=workbook, sheet_name=sheet_name) for sheet_name in sheet_names], ignore_index=True)
+
